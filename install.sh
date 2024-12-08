8dd0b9dd8ff9a38af6667475edd2f5607b6930e3
DM Kishi <dm.kishi@gmail.com>
2024-12-08 14:57:45 -0800

    feat: Add install script

--- install.sh
+++ install.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
+SRC="$(realpath src)"
+ln -sf "${SRC}/_editorconfig" ~/.editorconfig
+ln -sf "${SRC}/_hushlogin" ~/.hushlogin
+ln -sf "${SRC}/_tmux".conf ~/.tmux.conf
+ln -sf "${SRC}/_vimrc" ~/.vimrc
+ln -sf "${SRC}/_zshenv" ~/.zshenv
+ln -sf "${SRC}/_zshrc" ~/.zshrc
