934196d091480b805bfd6716c4ab12ef661c2878
DM Kishi <dm.kishi@gmail.com>
2024-12-10 15:06:49 -0800

    feat: Add tmux config to installer

--- install.sh
+++ install.sh
@@ -7,3 +7,6 @@ ln -sf "${DIR}/_tmux".conf ~/.tmux.conf
 ln -sf "${DIR}/_vimrc" ~/.vimrc
 ln -sf "${DIR}/_zshenv" ~/.zshenv
 ln -sf "${DIR}/_zshrc" ~/.zshrc
+
+tic -x "${DIR}/terminfo/tmux.terminfo"
+tic -x "${DIR}/terminfo/xterm-256color.terminfo"
